654f0758c7353ef0e634d2deaa16abb5c83c56bd
thiba <thibaut.audouy@etu.umontpellier.fr>
2024-03-26 11:53:01 +0100

    Service Tableau fait mais clairement pas testé. Plusieurs méthodes dans les repository à rajouter

--- src/Service/TableauService.php
+++ src/Service/TableauService.php
@@ -3,6 +3,8 @@
 use App\Trellotrolle\Lib\MotDePasseInterface;
 use App\Trellotrolle\Modele\DataObject\Tableau;
 use App\Trellotrolle\Modele\DataObject\Utilisateur;
+use App\Trellotrolle\Modele\Repository\CarteRepositoryInterface;
+use App\Trellotrolle\Modele\Repository\ColonneRepositoryInterface;
 use App\Trellotrolle\Modele\Repository\TableauRepositoryInterface;
 use App\Trellotrolle\Modele\Repository\UtilisateurRepositoryInterface;
 use App\Trellotrolle\Service\Exception\ServiceException;
@@ -12,6 +14,8 @@ class TableauService
 {
     public function __construct(private TableauRepositoryInterface $tableauRepository,
                                 private UtilisateurRepositoryInterface $utilisateurRepository,
+                                private CarteRepositoryInterface $carteRepository,
+                                private ColonneRepositoryInterface $colonneRepository,
                                 private MotDePasseInterface $motDePasse) {}
 
     /**
@@ -143,14 +147,91 @@ class TableauService
         if(is_null($utilisateurNouveau)){
             throw new ServiceException( "L'utilisateur à ajouter n'existe pas", Response::HTTP_NOT_FOUND);
         }
-        if($tableau->estParticipantOuProprietaire($loginUtilisateurNouveau)){
+        if($this->tableauRepository->estParticipantOuProprietaire($loginUtilisateurNouveau, $idTableau)){ // TODO: ajouter cette méthode dans les repository
             throw new ServiceException( "L'utilisateur est proprio ou participe déjà à ce talbeau", Response::HTTP_CONFLICT);
         }
 
-        $tab = $tableau->getParticipants(); // TODO: à revoir le système pour ajouter des membres, c'est plus comme ça dans la NEW BD
-        $tab[] = $loginUtilisateurNouveau;
-        $tableau->setParticipants($tab); // TODO : ajouter une fonciton ajouter membre dans DataObject et refactor ces 3 lignes içi
+        $this->tableauRepository->ajouterMembre($loginUtilisateurNouveau, $idTableau); // TODO : ajouter cette méthode dans les repository
+    }
 
-        $this->tableauRepository->mettreAJour($tableau);
+    /**
+     * @throws ServiceException
+     */
+    public function supprimerMembre(?int $idTableau, ?string $loginUtilisateurConnecte, ?string $loginUtilisateurDelete){ // Fonction à vrm tester
+        if(is_null($idTableau) ||is_null($loginUtilisateurConnecte) || strlen($loginUtilisateurConnecte) == 0 || is_null($loginUtilisateurDelete) || strlen($loginUtilisateurDelete) == 0){
+            throw new ServiceException( "L'idTableau ou le login de l'user connecté ou le login a ajouté ne peut pas être vide", Response::HTTP_BAD_REQUEST);
+        }
+
+        /**
+         * @var Tableau $tableau
+         */
+        $tableau = $this->tableauRepository->recupererParClePrimaire($idTableau);
+
+        if(is_null($tableau)){
+            throw new ServiceException( "Le tableau n'existe pas", Response::HTTP_NOT_FOUND);
+        }
+        // Si l'utilisateur connecté veut supprimer qq d'autre (seul le proprio peut supprimer dans ce cas)
+        if($loginUtilisateurConnecte != $loginUtilisateurDelete){
+            if(! $tableau->estProprietaire($loginUtilisateurConnecte)){
+                throw new ServiceException( "Seul le propriétaire du tableau peut mettre supprimer des membres", Response::HTTP_UNAUTHORIZED);
+            }
+        }
+        else{ // Ca signifie que l'utilisateur connecté est le même que celui à supprimer (il a le droite de quitter le tableau)
+            if($tableau->estProprietaire($loginUtilisateurDelete)){
+                throw new ServiceException( "Vous ne pouvez pas vous supprimer du tableau si vous êtes propriétaire", Response::HTTP_BAD_REQUEST);
+            }
+        }
+
+        /**
+         * @var Utilisateur $utilisateurNouveau
+         */
+        $utilisateurNouveau = $this->utilisateurRepository->recupererParClePrimaire($loginUtilisateurDelete);
+
+        if(is_null($utilisateurNouveau)){
+            throw new ServiceException( "L'utilisateur à supprimer n'existe pas", Response::HTTP_NOT_FOUND);
+        }
+        if(! $this->tableauRepository->estParticipantOuProprietaire($loginUtilisateurDelete, $idTableau)){ // TODO: ajouter cette méthode dans les repository. A voir si la fonction peut être simplifer (je pense que oui)
+            throw new ServiceException( "L'utilisateur ne participe pas à ce talbeau", Response::HTTP_CONFLICT);
+        }
+
+        $this->carteRepository->supprimerAffectation($idTableau, $loginUtilisateurDelete); // TODO: ajotuer cette méthode dans les repository ou tout du moins faire en sorte que supprimerMembre supprime les affectations aux Cartes
+        $this->tableauRepository->supprimerMembre($loginUtilisateurDelete, $idTableau); // TODO : ajouter cette méthode dans les repository
+    }
+
+    /**
+     * @throws ServiceException
+     */
+    public function recupererTableauxOuUtilisateurEstMembre(?string $loginUtilisateurConnecte){
+        if(is_null($loginUtilisateurConnecte) || strlen($loginUtilisateurConnecte) == 0){
+            throw new ServiceException( "Le login de l'user connecté ne peut pas être vide", Response::HTTP_BAD_REQUEST);
+        }
+
+        $utilisateur = $this->utilisateurRepository->recupererParClePrimaire($loginUtilisateurConnecte);
+
+        if(is_null($utilisateur)){
+            throw new ServiceException( "L'utilisateur n'existe pas", Response::HTTP_NOT_FOUND);
+        }
+
+        return $this->tableauRepository->recupererTableauxOuUtilisateurEstMembre($loginUtilisateurConnecte);
+    }
+
+    /**
+     * @throws ServiceException
+     */
+    public function supprimer(?string $loginUtilisateurConnecte, ?int $idTableau){
+        if(is_null($loginUtilisateurConnecte) || strlen($loginUtilisateurConnecte) == 0 || is_null($idTableau)){
+            throw new ServiceException( "L'idTableau ou le login de l'user connecté ne peut pas être vide", Response::HTTP_BAD_REQUEST);
+        }
+
+        $utilisateur = $this->utilisateurRepository->recupererParClePrimaire($loginUtilisateurConnecte);
+
+        if(is_null($utilisateur)){
+            throw new ServiceException( "L'utilisateur n'existe pas", Response::HTTP_NOT_FOUND);
+        }
+
+        $this->carteRepository->supprimerCartesTableau($idTableau); // TODO: ajouter cette méthode dans les repository ou alors faire ceci dans supprimer de tableauRepository ?
+        $this->colonneRepository->supprimerColonneTableau($idTableau); // TODO: ajouter cette méthode dans les repository ou alors faire ceci dans supprimer de tableauRepository ?
+        $this->tableauRepository->supprimerUtilisateurParticipant($idTableau); // TODO: ajouter cette méthode dans les repository ou alors faire ceci dans supprimer de tableauRepository ?
+        $this->tableauRepository->supprimer($idTableau);
     }
 }
