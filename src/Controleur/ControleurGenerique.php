527a3fb11affc068f494cd7007e3b90d9d3256db
Audouy Thibaut <thibaut.audouy@etu.umontpellier.fr>
2024-03-28 15:02:56 +0100

    Methode useless en moins

--- src/Controleur/ControleurGenerique.php
+++ src/Controleur/ControleurGenerique.php
@@ -6,15 +6,32 @@ use App\Trellotrolle\Lib\MessageFlash;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class ControleurGenerique {
 
     public function __construct(private ContainerInterface $container)
     {}
 
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws LoaderError
+     */
+    protected function afficherTwig(string $cheminVue, array $parametres = []): Response
+    {
+        /** @var Environment $twig */
+        $twig = $this->container->get("twig");
+        $corpsReponse = $twig->render($cheminVue, $parametres);
+        return new Response($corpsReponse);
+    }
+
     protected function afficherVue(string $cheminVue, array $parametres = []): Response {
         extract($parametres);
-        $messagesFlash = MessageFlash::lireTousMessages();
+        $messagesFlash = MessageFlash::lireTousMessages(); // TODO: injecter dépendance
         ob_start();
         require $this->container->getParameter('project_root'). "/src/vue/$cheminVue";
         $corpsReponse = ob_get_clean();
@@ -26,26 +43,6 @@ class ControleurGenerique {
         $generateurUrl = $this->container->get("url_generator");
         $url = $generateurUrl->generate($routeName, $parameters);
         return new RedirectResponse($url);
-    }
-
-    // https://stackoverflow.com/questions/768431/how-do-i-make-a-redirect-in-php
-    protected static function redirection(string $controleur = "", string $action = "", array $query = []) : void
-    {
-        $queryString = [];
-        if ($action != "") {
-            $queryString[] = "action=$action";
-        }
-        if ($controleur != "") {
-            $queryString[] = "controleur=$controleur";
-        }
-        foreach ($query as $name => $value) {
-            $name = rawurlencode($name);
-            $value = rawurlencode($value);
-            $queryString[] = "$name=$value";
-        }
-        $url = "Location: ./controleurFrontal.php?" . join("&", $queryString);
-        header($url);
-        exit();
     }
 
     public function afficherErreur($messageErreur = "", $controleur = ""): Response
