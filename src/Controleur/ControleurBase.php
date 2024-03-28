02cd64ffa037faf4a71a73acd09f4c035e08e1ef
francoisn <norman.francois@etu.umontpellier.fr>
2024-03-28 21:52:37 +0100

    refactor [AbstractRepository]: retrait de la route test

--- src/Controleur/ControleurBase.php
+++ src/Controleur/ControleurBase.php
@@ -2,8 +2,14 @@
 
 namespace App\Trellotrolle\Controleur;
 
+use App\Trellotrolle\Modele\DataObject\Carte;
+use App\Trellotrolle\Modele\DataObject\Colonne;
+use App\Trellotrolle\Modele\DataObject\Tableau;
+use App\Trellotrolle\Modele\DataObject\Utilisateur;
+use App\Trellotrolle\Modele\Repository\CarteRepository;
 use App\Trellotrolle\Modele\Repository\ColonneRepository;
 use App\Trellotrolle\Modele\Repository\TableauRepository;
+use App\Trellotrolle\Modele\Repository\UtilisateurRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -18,12 +24,4 @@ class ControleurBase extends ControleurGenerique
         ]);
     }
 
-    #[Route(path: '/test', name:'accueil', methods:["GET"])]
-    public function test(): Response
-    {
-        /*ob_start();
-        var_dump(;
-        $corpsReponse = ob_get_clean();*/
-        return new JsonResponse((new ColonneRepository())->recupererParClePrimaire(1));
-    }
 }
