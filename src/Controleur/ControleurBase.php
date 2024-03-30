466e2332a9eb49309a90ac5d206cf50b5c260aab
francoisn <norman.francois@etu.umontpellier.fr>
2024-03-30 04:36:04 +0100

    refactor [TableauControlleur]: retrait des tests

--- src/Controleur/ControleurBase.php
+++ src/Controleur/ControleurBase.php
@@ -2,7 +2,6 @@
 
 namespace App\Trellotrolle\Controleur;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -11,11 +10,6 @@ class ControleurBase extends ControleurGenerique
     #[Route(path: '/', name:'accueil', methods:["GET", "POST"])]
     public function accueil(): Response {
         return $this->afficherTwig("base/accueil.html.twig", ["pagetitle" => "Accueil"]);
-    }
-
-    #[Route(path: '/test', name:'test', methods:["GET", "POST"])]
-    public function test(): Response {
-        return new JsonResponse($this->container->get("tableau_service")->informationsAffectationsCartes('1'));
     }
 
 }
