8adfbc0cc74a43a1d24baa54ffd81b8d0a4ba70e
schaz_ <mathias.lemoine@etu.umontpellier.fr>
2024-04-01 19:35:44 +0200

    feat: docs lib + clean code

--- src/Lib/ConnexionUtilisateurSession.php
+++ src/Lib/ConnexionUtilisateurSession.php
@@ -2,34 +2,73 @@
 
 namespace App\Trellotrolle\Lib;
 
-use App\Trellotrolle\Lib\ConnexionUtilisateurInterface;
-use App\Trellotrolle\Modele\DataObject\Utilisateur;
 use App\Trellotrolle\Modele\HTTP\Session;
-use App\Trellotrolle\Modele\Repository\UtilisateurRepository;
 
+
+/**
+ * Classe ConnexionUtilisateurSession
+ *
+ * Cette classe implémente l'interface ConnexionUtilisateurInterface et gère la connexion de l'utilisateur via une session.
+ * Elle utilise la classe Session pour gérer la session utilisateur.
+ *
+ * @package App\Trellotrolle\Lib
+ */
 class ConnexionUtilisateurSession implements ConnexionUtilisateurInterface
 {
+    /**
+     * Variable privée représentant la clé de connexion de l'utilisateur.
+     *
+     * @var string $cleConnexion
+     */
     private string $cleConnexion = "_utilisateurConnecte";
 
-    // Note : Classe trop couplée avec les sessions
+    /**
+     * Méthode connecter
+     *
+     * Cette méthode permet de connecter un utilisateur en enregistrant son identifiant dans la session.
+     *
+     * @param string $idUtilisateur L'identifiant de l'utilisateur à connecter
+     * @return void
+     */
     public function connecter(string $idUtilisateur): void
     {
         $session = Session::getInstance();
         $session->enregistrer($this->cleConnexion, $idUtilisateur);
     }
 
+    /**
+     * Méthode estConnecte
+     *
+     * Cette méthode vérifie si l'utilisateur est connecté en vérifiant si la session contient la clé de connexion.
+     *
+     * @return bool Retourne true si l'utilisateur est connecté, sinon false.
+     */
     public function estConnecte(): bool
     {
         $session = Session::getInstance();
         return $session->contient($this->cleConnexion);
     }
 
-    public function deconnecter()
+    /**
+     * Méthode deconnecter
+     *
+     * Cette méthode permet de déconnecter l'utilisateur en supprimant la clé de connexion de la session.
+     *
+     * @return void
+     */
+    public function deconnecter(): void
     {
         $session = Session::getInstance();
         $session->supprimer($this->cleConnexion);
     }
 
+    /**
+     * Méthode getIdUtilisateurConnecte
+     *
+     * Cette méthode permet d'obtenir l'identifiant de l'utilisateur connecté en vérifiant la clé de connexion dans la session.
+     *
+     * @return string|null Retourne l'identifiant de l'utilisateur connecté s'il existe, sinon retourne null.
+     */
     public function getIdUtilisateurConnecte(): ?string
     {
         $session = Session::getInstance();
--- src/Lib/JsonWebToken.php
+++ src/Lib/JsonWebToken.php
@@ -2,22 +2,51 @@
 
 namespace App\Trellotrolle\Lib;
 
+use Exception;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
+/**
+ * Classe JsonWebToken
+ *
+ * Cette classe est responsable de l'encodage et du décodage des JSON Web Tokens (JWT).
+ */
 class JsonWebToken
 {
+    /**
+     * Variable $jsonSecret
+     *
+     * Cette variable est une chaîne de caractères qui représente la clé secrète utilisée pour l'encodage et le décodage des JSON Web Tokens (JWT).
+     */
     private static string $jsonSecret = "cQTcHTyBBwfGaFkCez0EJ6";
 
-    public static function encoder(array $contenu) : string {
+    /**
+     * Méthode encoder
+     *
+     * Cette méthode est responsable de l'encodage d'un tableau en JSON Web Token (JWT).
+     *
+     * @param array $contenu Le tableau à encoder
+     * @return string Le JWT encodé
+     */
+    public static function encoder(array $contenu): string
+    {
         return JWT::encode($contenu, self::$jsonSecret, 'HS256');
     }
 
-    public static function decoder(string $jwt) : array {
+    /**
+     * Méthode decoder
+     *
+     * Cette méthode est responsable du décodage d'un JSON Web Token (JWT) en un tableau.
+     *
+     * @param string $jwt Le JWT à décoder
+     * @return array Le tableau décodé
+     */
+    public static function decoder(string $jwt): array
+    {
         try {
             $decoded = JWT::decode($jwt, new Key(self::$jsonSecret, 'HS256'));
-            return (array) $decoded;
-        } catch (\Exception $exception) {
+            return (array)$decoded;
+        } catch (Exception) {
             return [];
         }
     }
