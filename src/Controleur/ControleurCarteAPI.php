c5737c6db2a5b16e81e69b908d23a0a70b7bcbe2
thiba <thibaut.audouy@etu.umontpellier.fr>
2024-04-01 22:23:05 +0200

    Feat controleur Carte API fait

--- src/Controleur/ControleurCarteAPI.php
+++ src/Controleur/ControleurCarteAPI.php
@@ -4,6 +4,7 @@ namespace App\Trellotrolle\Controleur;
 
 use App\Trellotrolle\Lib\ConnexionUtilisateurInterface;
 use App\Trellotrolle\Service\CarteServiceInterface;
+use App\Trellotrolle\Service\ColonneServiceInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use App\Trellotrolle\Lib\MessageFlash;
@@ -50,19 +51,46 @@ class ControleurCarteAPI extends ControleurGenerique
         }
     }
 
-    /*#[Route(path: '/api/cartes', name:'api_creer_carte', methods:["POST"])]
+    #[Route(path: '/api/cartes', name:'api_creer_carte', methods:["POST"])]
     public function creerCarte(Request $request) : Response { // Fonctionne
         if(! $this->estConnecte()){
             return new JsonResponse(["error" => "Vous devez "], Response::HTTP_UNAUTHORIZED);
         }
         try {
             $jsonObject = json_decode($request->getContent(), flags: JSON_THROW_ON_ERROR);
+            $idColonne = $jsonObject->idColonne;
+            $titreCarte = $jsonObject->titreCarte;
+            $descriptifCarte = $jsonObject->descriptifCarte;
+            $couleurCarte = $jsonObject->couleurCarte;
+            $affectationsCarte = $jsonObject->affectationsCarte;
+            $idCarte = $this->carteService->creerCarte($idColonne, $titreCarte, $descriptifCarte, $couleurCarte, $this->connexionUtilisateurJWT->getIdUtilisateurConnecte(), $affectationsCarte);
 
-            $this->carteService->creerCarte($this->connexionUtilisateurJWT->getIdUtilisateurConnecte());
-            return new JsonResponse(true, Response::HTTP_OK); // True si ça a été supprimé
+            $carte = $this->carteService->getCarte($idCarte);
+            return new JsonResponse($carte, Response::HTTP_OK); // Renvoie la carte
         } catch (Exception $exception) {
             return new JsonResponse(["error" => $exception->getMessage()], $exception->getCode());
         }
-    }*/
+    }
+
+    #[Route(path: '/api/cartes', name:'api_modifier_carte', methods:["PATCH"])]
+    public function mettreAJour(Request $request) : Response { // Fonctionne
+        if(! $this->estConnecte()){
+            return new JsonResponse(["error" => "Vous devez "], Response::HTTP_UNAUTHORIZED);
+        }
+        try {
+            $jsonObject = json_decode($request->getContent(), flags: JSON_THROW_ON_ERROR);
+            $idCarte = $jsonObject->idCarte;
+            $idColonne = $jsonObject->idColonne;
+            $titreCarte = $jsonObject->titreCarte;
+            $descriptifCarte = $jsonObject->descriptifCarte;
+            $couleurCarte = $jsonObject->couleurCarte;
+            $affectationsCarte = $jsonObject->affectationsCarte;
+
+            $carte = $this->carteService->mettreAJourCarte($idCarte, $idColonne,$titreCarte,$descriptifCarte,$couleurCarte, $this->connexionUtilisateurJWT->getIdUtilisateurConnecte(), $affectationsCarte);
+            return new JsonResponse($carte, Response::HTTP_OK); // Renvoie la colonne avec la carte crée en plus
+        } catch (Exception $exception) {
+            return new JsonResponse(["error" => $exception->getMessage()], $exception->getCode());
+        }
+    }
 
 }
