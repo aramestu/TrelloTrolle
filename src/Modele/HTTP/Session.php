6461ceb54e3aa0e1dc1cef6ed87e857064ec80ff
schaz_ <mathias.lemoine@etu.umontpellier.fr>
2024-04-01 20:14:59 +0200

    feat: docs HTTP + clean code

--- src/Modele/HTTP/Session.php
+++ src/Modele/HTTP/Session.php
@@ -3,13 +3,25 @@
 namespace App\Trellotrolle\Modele\HTTP;
 
 use App\Trellotrolle\Configuration\ConfigurationSite;
-use App\Trellotrolle\Lib\ConnexionUtilisateur;
 use Exception;
 
+/**
+ * Classe Session
+ *
+ * Cette classe gère la session en cours.
+ * Elle permet de démarrer la session, vérifier la dernière activité, accéder à l'instance de la session, vérifier si un nom existe dans la session, enregistrer une valeur dans la session, lire une valeur de la session, supprimer une valeur de la session et détruire la session.
+ * La classe utilise la classe ConfigurationSite pour obtenir la durée d'expiration de la session.
+ */
 class Session
 {
     private static ?Session $instance = null;
 
+    /**
+     * Constructeur de la classe Session.
+     * Démarre la session en cours.
+     * Lance une exception si la session ne peut pas démarrer.
+     * @throws Exception
+     */
     private function __construct()
     {
         if (session_start() === false) {
@@ -17,7 +29,18 @@ class Session
         }
     }
 
-    public function verifierDerniereActivite(int $dureeExpiration) : void
+    /**
+     * Méthode verifierDerniereActivite
+     *
+     * Cette méthode vérifie la dernière activité de la session en cours.
+     * Si la durée d'expiration de la session est différente de zéro et la dernière activité dépasse cette durée,
+     * la session est réinitialisée.
+     * La méthode met également à jour la valeur de la dernière activité de la session.
+     *
+     * @param int $dureeExpiration La durée d'expiration de la session en secondes
+     * @return void
+     */
+    public function verifierDerniereActivite(int $dureeExpiration): void
     {
         if ($dureeExpiration == 0)
             return;
@@ -26,9 +49,17 @@ class Session
             session_unset();
 
         $_SESSION['derniereActivite'] = time();
-
     }
 
+    /**
+     * Méthode getInstance
+     *
+     * Cette méthode retourne une instance de la classe Session.
+     * Si l'instance n'existe pas, elle est créée en utilisant le constructeur de la classe Session.
+     * La méthode vérifie également la durée d'expiration de la session en utilisant la méthode getDureeExpirationSession de la classe ConfigurationSite.
+     *
+     * @return Session L'instance de la classe Session
+     */
     public static function getInstance(): Session
     {
         if (is_null(static::$instance)) {
@@ -41,35 +72,74 @@ class Session
         return static::$instance;
     }
 
-    public function contient($nom): bool
+    /**
+     * Méthode contient
+     *
+     * Cette méthode vérifie si une clé existe dans la session en cours.
+     *
+     * @param string $nom Le nom de la clé à vérifier
+     * @return bool Retourne true si la clé existe dans la session, sinon false
+     */
+    public function contient(string $nom): bool
     {
         return isset($_SESSION[$nom]);
     }
 
+    /**
+     * Méthode enregistrer
+     *
+     * Cette méthode enregistre une valeur dans la session en cours.
+     *
+     * @param string $nom Le nom de la clé dans la session
+     * @param mixed $valeur La valeur à enregistrer dans la session
+     * @return void
+     */
     public function enregistrer(string $nom, mixed $valeur): void
     {
         $_SESSION[$nom] = $valeur;
     }
 
+    /**
+     * Méthode lire
+     *
+     * Cette méthode permet de lire une valeur dans la session en cours.
+     *
+     * @param string $nom Le nom de la clé dans la session
+     * @return mixed La valeur correspondante dans la session
+     */
     public function lire(string $nom): mixed
     {
-        return $_SESSION[$nom];
+        if ($this->contient($nom)) return $_SESSION[$nom];
+        return null;
     }
 
-    public function supprimer($nom): void
+    /**
+     * Méthode supprimer
+     *
+     * Cette méthode supprime une clé de la session en cours.
+     *
+     * @param string $nom Le nom de la clé à supprimer
+     * @return void
+     */
+    public function supprimer(string $nom): void
     {
         unset($_SESSION[$nom]);
     }
-    public function detruire() : void
+
+    /**
+     * Méthode detruire
+     *
+     * Cette méthode détruit la session en cours.
+     * Elle supprime toutes les variables de session, détruit la session elle-même,
+     * supprime le cookie de session et réinitialise l'instance de la classe Session à null.
+     *
+     * @return void
+     */
+    public function detruire(): void
     {
         session_unset();
         session_destroy();
         Cookie::supprimer(session_name());
         Session::$instance = null;
-    }
-
-    public function telemetry($a, $b, $c)
-    {
-        ConnexionUtilisateur::important($a, $b ? null : (($c+$a) > $a*$a ? $b : 24));
     }
 }
