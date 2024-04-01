75f24d4f4605977a5a80ab55513a8a3ef2da443f
schaz_ <mathias.lemoine@etu.umontpellier.fr>
2024-04-01 19:14:45 +0200

    fix: no context

--- src/Test/CarteServiceTest.php
+++ src/Test/CarteServiceTest.php
@@ -20,20 +20,11 @@ use PDO;
 //Base de donnees deja rempli d'import
 class CarteServiceTest extends TestCase
 {
-
-    private static ConnexionBaseDeDonneesInterface $connexionBaseDeDonnees;
-
     protected CarteRepositoryInterface $carteRepositoryMock;
     protected ColonneServiceInterface $colonneServiceMock;
     protected TableauServiceInterface $tableauServiceMock;
     protected CarteService $carteService;
 
-    public static function setUpBeforeClass(): void
-    {
-        parent::setUpBeforeClass();
-        self::$connexionBaseDeDonnees = new ConnexionBaseDeDonnees(new ConfigurationBDDTest());
-        //self::$connexionBaseDeDonnees->getPdo()->exec(file_get_contents(__DIR__."/BD_Tables_V1.sql")); A décommenter si les tables n'éxiste plus dans la BDD de test
-    }
 
     /**
      * @throws Exception
@@ -62,20 +53,6 @@ class CarteServiceTest extends TestCase
         parent::tearDown();
     }
 
-    public function testScriptCreationTables(): void
-    {
-        // Assurez-vous que la connexion à la base de données est établie
-        self::$connexionBaseDeDonnees->getPdo()->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        // Vérifiez si la table "cartes" existe dans la base de données
-        $stmt = self::$connexionBaseDeDonnees->getPdo()->query("SELECT 1 FROM cartes LIMIT 1");
-
-        // Assurez-vous que la requête s'est exécutée sans erreur
-        $this->assertNotFalse($stmt, "La table 'cartes' n'a pas été créée.");
-
-        // Facultatif : affichage d'un message de réussite
-        $this->assertTrue(true, "Le script de création de tables s'est exécuté avec succès.");
-    }
 
     /**
      * @throws Exception
@@ -98,80 +75,4 @@ class CarteServiceTest extends TestCase
         // Assert
         $this->assertEquals($carte, $result);
     }
-
-
-    /**
-     * @throws ServiceException
-     */
-    public function testMettreAJourCarte()
-    {
-        $statement = self::$connexionBaseDeDonnees->getPdo()->query("SELECT * FROM cartes where idCarte = 1");
-        $carteObject = $statement->fetch(PDO::FETCH_ASSOC);
-
-        $statement = self::$connexionBaseDeDonnees->getPdo()->query("SELECT * FROM colonnes where idcolonne ='" . $carteObject['idcolonne'] . "'");
-        $colonneObject = $statement->fetch(PDO::FETCH_ASSOC);
-
-        $statement = self::$connexionBaseDeDonnees->getPdo()->query("SELECT * from tableaux where idtableau='" . $colonneObject["idtableau"]. "'");
-        $tableauObject = $statement->fetch(PDO::FETCH_ASSOC);
-
-        $statement = self::$connexionBaseDeDonnees->getPdo()->query("SELECT * FROM affecter where login='" . $tableauObject['proprietaireTableau'] . "'");
-        $affected = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-
-        var_dump($carteObject);
-        var_dump($tableauObject);
-
-        $carte = new Carte();
-        $carte->setIdCarte($carteObject["idcarte"]);
-        $carte->setAffectationsCarte($affected);
-        $carte->setCouleurCarte($carteObject["couleurcarte"]);
-        $carte->setDescriptifCarte($carteObject["descriptifcarte"]);
-        $carte->setTitreCarte($carteObject["titrecarte"]);
-
-
-        $colonne = new Colonne();
-        $colonne->setIdColonne($carteObject["idcolonne"]);
-        $colonne->setTitreColonne($colonneObject["titrecolonne"]);
-
-
-        $carte->setColonne($colonne);
-
-        $tableau = new Tableau();
-        $tableau->setIdTableau($tableauObject['idtableau']);
-        $tableau->setCodeTableau($tableauObject["codetableau"]);
-        $tableau->setTitreTableau($tableauObject["titretableau"]);
-
-        $colonne->setTableau($tableau);
-
-        $this->colonneServiceMock->expects($this->once())
-            ->method('getColonne')
-            ->with($colonne->getIdColonne())
-            ->willReturn($colonne);
-
-        $this->tableauServiceMock->expects($this->once())
-            ->method('getByIdTableau')
-            ->with($tableau->getIdTableau())
-            ->willReturn($tableau);
-
-        $this->carteRepositoryMock->expects($this->once())
-            ->method('mettreAJour')
-            ->with($carte);
-
-        $this->carteService->mettreAJourCarte($carte->getIdCarte(),$colonne->getIdColonne(), $carte->getTitreCarte(),$carte->getDescriptifCarte(), $carte->getCouleurCarte(),"utilisateur1", $affected);
-    }
-
-    /*public function testCreerCarte()
-    {
-
-    }
-
-    public function testGetCartesParIdColonne()
-    {
-
-    }
-
-    public function testSupprimerCarte()
-    {
-
-    }*/
 }
