d687e61ace1742015887a9bfe125500927e21ee7
schaz_ <mathias.lemoine@etu.umontpellier.fr>
2024-04-01 19:46:57 +0200

    feat: docs configuration + clean code

--- src/Configuration/ConfigurationSite.php
+++ src/Configuration/ConfigurationSite.php
@@ -2,7 +2,16 @@
 
 namespace App\Trellotrolle\Configuration;
 
+/**
+ *  Classe ConfigurationSite
+ *
+ *  Cette classe contient une méthode statique getDureeExpirationSession() qui retourne la durée d'expiration de la session en secondes.
+ */
 class ConfigurationSite {
+
+    /**
+     * @return int La durée d'expiration de la session en secondes.
+     */
     static public function getDureeExpirationSession() : int {
         return 36000;
     }
